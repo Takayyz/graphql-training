5d019cccff5c156929485c6017f774dfbe78e531
Takayyz <like.the.wind0306@gmail.com>
2024-02-28 09:30:12 +0900

    chore: install GraphQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hey, there!');
+import { createYoga } from 'graphql-yoga';
+import { createServer } from 'node:http';
+import { schema } from './shema';
+
+const port = Number(process.env.API_PORT) || 4000
+
+const yoga = createYoga({ schema });
+
+const server = createServer(yoga);
+
+server.listen(() => {
+  console.info(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
